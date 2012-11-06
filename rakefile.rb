ef0b6f068a4f96b1a3d046f0b6d0e25217f658f2
Morris Feldman <morrifeldman@gmail.com>
2012-11-06 17:04:37 +0200

    Enable mps/ps and compute all_tp_avg.bed

--- rakefile.rb
+++ rakefile.rb
@@ -8,6 +8,7 @@ gse = 'GSE24391'
 gse_url = "http://www.ncbi.nlm.nih.gov/geosuppl/?acc=#{gse}"
 
 probe_category = 'comprehensive'
+ps_or_mps = 'mps'
 
 CLOBBER.include(probe_category, '*.CEL', '*.gz', '*_*', 'replicates.txt')
 CLOBBER.include("#{gse}.tar")
@@ -17,7 +18,7 @@ celFiles = File.join(celDir,'*.CEL')
 libBase = 'HuEx-1_0-st-v2.r2'
 pgf = libBase + '.pgf'
 clf = libBase + '.clf'
-ps = "HuEx-1_0-st-v2.r2.dt1.hg18.#{probe_category}.ps"
+ps_or_mps_lib = "#{libBase}.dt1.hg18.#{probe_category}.#{ps_or_mps}"
 qcc = libBase + '.qcc'
 bgp = libBase + '.antigenomic.bgp'
 apt = 'apt-probeset-summarize'
@@ -59,7 +60,7 @@ file tar_file do |f|
   sh "curl -o #{f.name} #{gse_url}"
 end
 
-file replicates[0] => tar_file do
+file cel_files[0] => tar_file do
   sh "tar xvf #{gse}.tar"
   sh "gunzip -v *.gz"
   file_hash.each do |orig_name, new_name|
@@ -68,17 +69,20 @@ file replicates[0] => tar_file do
   end
 end
 
-cel_file_txt = 'replicates.txt'
+ps_or_tc = ps_or_mps == 'ps' ? 'ps' : 'tc'
+
+cel_file_txt = 'cel_files.txt'
 file "#{probe_category}/rma-sketch.summary.txt" => replicates[0] do
   File.open(cel_file_txt,'w') do |io|
     io << "cel_files\n"
     io << replicates.join("\n")
   end
-  cmd = "#{apt} -a #{action} -p #{pgf} -c #{clf} -s #{ps} -b #{bgp} -o #{probe_category} --qc-probesets #{qcc} --cel-files #{cel_file_txt}"
+  ps_or_mps_flag = ps_or_mps == 'ps' ? "-s" : "-m"
+  cmd = "#{apt} -a #{action} -p #{pgf} -c #{clf} #{ps_or_mps_flag} #{ps_or_mps_lib} -b #{bgp} -o #{probe_category} --qc-probesets #{qcc} --cel-files #{cel_file_txt}"
   sh cmd
 end
 
-file "#{probe_category}/expr.ps" => "#{probe_category}/rma-sketch.summary.txt" do |f|
+file "#{probe_category}/expr.#{ps_or_tc}" => "#{probe_category}/rma-sketch.summary.txt" do |f|
   sh "removeAffyHeader.rb -f #{f.prerequisites[0]} -o #{f.name}"
 end
 
@@ -99,13 +103,13 @@ def avg_line_reps(data_row_ar, cond_hash)
 end
 
 
-def average_affy_replicates(in_file_name, out_file_name)
+def average_replicates(in_file_name, out_file_name)
   File.open(in_file_name) do |f_in|
     header = f_in.gets.split
     cond_hash = parse_header(header[1..-1])
     cond_hash.each {|cond, cols| puts "#{cond} => #{cols}"}
     File.open(out_file_name, 'w') do |f_out|
-      f_out << "ps\t" << cond_hash.keys.join("\t") << "\n"
+      f_out << "ps_or_tc\t" << cond_hash.keys.join("\t") << "\n"
       f_in.each.with_progress('Averaging Replicates') do |line|
         line_ar = line.split
         out_ar = [line_ar.shift]
@@ -116,8 +120,8 @@ def average_affy_replicates(in_file_name, out_file_name)
   end
 end
 
-file "#{probe_category}/rep_average.ps" => "#{probe_category}/expr.ps" do |f|
-  average_affy_replicates(f.prerequisites[0], f.name)
+file "#{probe_category}/rep_average.#{ps_or_tc}" => "#{probe_category}/expr.#{ps_or_tc}" do |f|
+  average_replicates(f.prerequisites[0], f.name)
 end
 
 def add_bed_header(file_io, name, desc)
@@ -164,7 +168,7 @@ def bed_name(cond, probe_category)
 end
 
 example_bed = "#{probe_category}/#{conditions[0]}.bed"
-file example_bed => "#{probe_category}/rep_average.ps" do |f|
+file example_bed => "#{probe_category}/rep_average.#{ps_or_tc}" do |f|
   conditions.each do |cond|
     track_name = "raw_#{cond}"
     track_desc = "Raw Expression Values for #{cond}, #{probe_category} probesets"
@@ -172,6 +176,41 @@ file example_bed => "#{probe_category}/rep_average.ps" do |f|
     bed_from_ps(f.prerequisites[0], cond, out_file_name, track_name, track_desc)
     sh "gzip -cv #{out_file_name} > #{out_file_name}.gz"
   end
+end
+
+def form_avg_bed_line(line, dict)
+  line_ar = line.split
+  ps = line_ar.shift
+  mean = line_ar.map{|e| e.to_f}.mean
+  coord = dict[ps]
+  coord.nil? ? '' : "#{coord}\t#{mean}\n"
+end
+
+def fill_avg_bed(in_file_name, out_file_io)
+  puts "Loading Dictionary"
+  dict = readKyotoHash('ps2bed')
+  File.open(in_file_name) do |f_in|
+    header = f_in.gets.split
+    progress_msg = "Filling Average Bed from #{in_file_name}"
+    f_in.each.with_progress(progress_msg) do |line|
+      out_file_io << form_avg_bed_line(line, dict)
+    end
+  end
+end
+
+def make_average_bed(in_file_name, out_file_name, track_name, track_desc)
+  track_name ||= in_file_name
+  File.open(out_file_name, 'w') do |out_file_io|
+    add_bed_header(out_file_io, track_name, track_desc)
+    fill_avg_bed(in_file_name, out_file_io)
+  end
+end
+
+mean_bed = "#{probe_category}/all_tp_mean.bed"
+file mean_bed => "#{probe_category}/expr.#{ps_or_tc}" do |f|
+  track_name = "raw_tp_mean"
+  track_desc = "Raw Expression Values Averaged Across All Time Points, #{probe_category}.#{ps_or_mps}"
+  make_average_bed(f.prerequisites[0], f.name, track_name, track_desc)
 end
 
 multi_bed_file = "#{probe_category}/raw_multi.bed"
@@ -183,4 +222,4 @@ file "#{multi_bed_file}.gz" => example_bed do
   sh "gzip -v #{multi_bed_file}"
 end
 
-task :default => "#{multi_bed_file}.gz"
+task :default => ["#{multi_bed_file}.gz", mean_bed]
